86dca5db1f628500dd1cc6d2dd5f66009337c350
mdmirajulkarim <mdmirajulkarim@gmail.com>
2019-09-05 01:56:19 +0600

    experience added

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -54,6 +54,7 @@ router.post(
       bio,
       status,
       githubusername,
+      handle,
       skills,
       youtube,
       facebook,
@@ -72,22 +73,20 @@ router.post(
     if (bio) profileFields.bio = bio;
     if (status) profileFields.status = status;
     if (githubusername) profileFields.githubusername = githubusername;
+    if (handle) profileFields.handle = handle;
     if (skills) {
       profileFields.skills = skills.split(",").map(skill => skill.trim());
     }
 
     // bUILD SOCIAL OBJECT
 
-    profileFields.social ={};
+    profileFields.social = {};
 
-    if(youtube) profileFields.social.youtube = youtube;
-    if(twitter) profileFields.twitter = twitter;
-    if(facebook) profileFields.facebook = facebook;
-    if(linkedin) profileFields.linkedin = linkedin;
-    if(instagram) profileFields.instagram = instagram;
-
-
-
+    if (youtube) profileFields.social.youtube = youtube;
+    if (twitter) profileFields.twitter = twitter;
+    if (facebook) profileFields.facebook = facebook;
+    if (linkedin) profileFields.linkedin = linkedin;
+    if (instagram) profileFields.instagram = instagram;
 
     try {
       let profile = await Profile.findOneAndUpdate(
@@ -97,6 +96,47 @@ router.post(
       );
       res.json(profile);
     } catch (error) {
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+// @router PUT api/profile/experience
+// @desc Add profile experience
+// @access Private
+
+router.put(
+  "/experience",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    const {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description,
+      status
+    } = req.body;
+
+    const newExp = {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    };
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+      profile.experience.unshift(newExp);
+      await profile.save();
+      res.json(profile);
+    } catch (error) {
+      console.log(error);
       res.status(500).send("Server Error");
     }
   }
