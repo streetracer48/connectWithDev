0cd6df98acf52b305b9c5d0a3ed76fe71ccc2840
mdmirajulkarim <mdmirajulkarim@gmail.com>
2019-09-16 21:17:16 +0600

    experience data show in dashboard

---
+++ client/src/components/dashboard/Experience.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import Moment from "react-moment";
+class Experience extends Component {
+  onDelete = () => {};
+  render() {
+    const { experience } = this.props;
+    let experienceContent;
+
+    if (experience.length > 0) {
+      experienceContent = this.props.experience.map(exp => (
+        <tr>
+          <td>{exp.company}</td>
+          <td>{exp.title}</td>
+          <td>
+            <Moment format="YYYY/MM/DD">{exp.from}</Moment> -
+            {exp.to === null ? (
+              "Now"
+            ) : (
+              <Moment format="YYYY/MM/DD">{exp.to}</Moment>
+            )}
+          </td>
+          <td>
+            <button onClick={this.onDelete} className="btn btn-danger">
+              Delete
+            </button>
+          </td>
+        </tr>
+      ));
+    } else {
+      experienceContent = <p>You have not yet add your experiences</p>;
+    }
+    return (
+      <div>
+        <h4 className="mb-4">Experience Credentials</h4>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Company</th>
+              <th>Title</th>
+              <th>Year</th>
+              <th />
+            </tr>
+            {experienceContent}
+          </thead>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default Experience;
